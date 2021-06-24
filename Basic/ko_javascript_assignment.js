52f1cc1a398293b6b680a482e9f09c837fe3b041
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-24 23:41:22 +0900

    Updated ko_javascript_assignment.js

--- Basic/ko_javascript_assignment.js
+++ Basic/ko_javascript_assignment.js
@@ -357,3 +357,83 @@ ask(
   () => alert("동의하셨습니다."),
   () => alert("취소 버튼을 누르셨습니다.")
 );
+
+// 객체야 안녕?
+// 다음 각 동작을 한 줄씩, 코드로 작성해보세요.
+
+// 빈 객체 user를 만듭니다.
+let user = {};
+// user에 키가 name, 값이 John인 프로퍼티를 추가하세요.
+user["name"] = "John";
+// user에 키가 surname, 값이 Smith인 프로퍼티를 추가하세요.
+user.surname = "Smith";
+// name의 값을 Pete로 수정해보세요.
+user.name = "Pete";
+// user에서 프로퍼티 name을 삭제하세요.
+delete user.name;
+
+// 객체가 비어있는지 확인하기
+// 객체에 프로퍼티가 하나도 없는 경우 true,
+//그렇지 않은 경우 false를 반환해주는 함수 isEmpty(obj)를 만들어 보세요.
+//아래와 같이 동작해야 합니다.
+let schedule = {};
+alert(isEmpty(schedule)); // true
+schedule["8:30"] = "get up";
+alert(isEmpty(schedule)); // false
+
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
+
+// 프로퍼티 합계 구하기
+// 모든 팀원의 월급에 대한 정보를 담고 있는 객체가 있다면,
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+// 모든 팀원의 월급을 합한 값을 구하고, 그 값을 변수 sum에 저장해주는 코드를 작성해보세요. sum은 390이어야함
+// 주의: salaries가 비어있다면, sum에 0이 저장되어야합니다.
+let sum = 0;
+for (let key in salaries) {
+  sum = sum + salareis[key];
+}
+
+// 프로퍼티 값 두 배로 부풀리기
+// 객체 obj의 프로퍼티 값이 숫자인 경우 그 값을 두 배 해주는 함수 multiplyNumeric(obj)을 만들어보세요/
+// 함수 호출 전
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+multiplyNumeric(menu);
+
+// 함수 호출 후
+menu = {
+  width: 400,
+  height: 600,
+  title: "My menu",
+};
+// multiplyNumeric은 아무것도 반환하지 않아도 괜찮습니다. 객체 자체를 수정해주기만 하면 됩니다.
+// 힌트) typeof를 사용하면 프로퍼티 값이 숫자인지 확인할 수 있습니다.
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "number") {
+      obj[key] = obj[key] * 2;
+    }
+  }
+}
+
+multiplyNumeric(menu);
