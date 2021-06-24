af0a5704da4ed12fb7a111f3b34156e21bc54196
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-25 02:05:07 +0900

    Updated ko_javascript_assignment.js

--- Basic/ko_javascript_assignment.js
+++ Basic/ko_javascript_assignment.js
@@ -437,3 +437,85 @@ function multiplyNumeric(obj) {
 }
 
 multiplyNumeric(menu);
+
+// 수를 입력받아 덧셈하기
+// 사용자에게 두 수를 입력받고, 두 수의 합을 출력해주는 스크립트를 작성해보세요.
+let a = +prompt("숫자를 알려주세요", "");
+let b = +prompt("숫자를 알려주세요", "");
+
+alert(a + b);
+
+// 숫자를 입력할 때까지 반복하기
+// 사용자가 유효한 숫자형 값을 입력할 때까지 계속 입력받는 함수 readNumber 를 만들어보세요.
+// 반환되는 값은 꼭 숫자형 값이어야 합니다.
+// 사용자가 아무 입력도 하지 않거나 '취소’를 누르면 입력받기를 멈추고 null을 반환하세요.
+
+function readNumber() {
+  let num;
+
+  do {
+    num = prompt("Enter a number please?", 0);
+  } while (!isFinite(num));
+
+  if (num === null || num === "") return null;
+
+  return +num;
+}
+
+alert(`Read: ${readNumber()}`);
+
+// 첫 글자를 대문자로 변경하기
+// str의 첫 글자를 대문자로 바꿔 반환하는 함수, ucFirst(str)를 만들어보세요. 함수 실행 결과는 아래 예시를 충족해야 합니다.
+ucFirst("john") == "John";
+function ucFirst(str) {
+  if (str.length === 0) {
+    return "";
+  } else {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+}
+
+// 스팸 문자열 걸러내기
+// str에 'viagra’나 'XXX’라는 문자열이 있으면 true를 반환해주는 함수 checkSpam(str)을 만들어보세요. 해당 문자열이 없으면 false를 반환하면 됩니다.
+
+function checkSpam(str) {
+  let newStr = str.toUpperCase();
+  if (newStr.includes("VIAGRA") || newStr.includes("XXX")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// 문자열 줄이기
+// str의 길이를 확인하고, 최대 길이 maxlength를 초과하는 경우 str의 끝을 생략 부호 ("…")로 대체해주는 함수 truncate(str, maxlength)를 만들어봅시다.
+// 새로 만든 문자열의 길이는 maxlength가 되어야 합니다.
+// 함수의 반환 값은 원하는 길이로 줄여진 문자열이 되어야 합니다.
+
+function truncate(str, maxlength) {
+  if (str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + "…";
+  } else {
+    return str;
+  }
+}
+
+// 숫자만 추출하기
+// 달러 표시가 먼저 나오고 그 뒤에 숫자가 나오는 문자열 "$120"가 있다고 가정해 봅시다.
+// 위와 같은 문자열에서 숫자만 뽑아내는 함수 extractCurrencyValue(str)를 작성해 봅시다.
+// 실행 결과는 아래와 같아야 합니다.
+
+function extractCurrencyValues(str) {
+  let number = "1234567890";
+  let result = "";
+  for (let i = 0; i < number.length; i++) {
+    if (str.includes(number[i])) {
+      result = result + number[i];
+    }
+  }
+  return Number(result);
+}
+
+function extractCurrencyValue(str) {
+  return +str.slice(1);
+}
