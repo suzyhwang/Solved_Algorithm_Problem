f3547b7d5d45b816e4da1e62a35c439a5219c6b8
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-25 23:39:56 +0900

    Updated ko_javascript_assignment.js

--- Basic/ko_javascript_assignment.js
+++ Basic/ko_javascript_assignment.js
@@ -519,3 +519,108 @@ function extractCurrencyValues(str) {
 function extractCurrencyValue(str) {
   return +str.slice(1);
 }
+
+// 배열은 복사가 될까요?
+let fruits = ["사과", "배", "오렌지"];
+// 배열을 '복사'한 후, push 메서드를 이용해 새로운 값을 추가합니다.
+let shoppingCart = fruits;
+shoppingCart.push("바나나");
+
+// fruits에 어떤 값이 들어 있을까요?
+alert(fruits.length); // 4
+// 배열은 객체이기 때문에 shoppingCart와 fruits는 모두 같은 배열을 참조합니다.
+
+// 배열과 관련된 연산
+// 배열과 관련된 다섯 가지 연산을 해봅시다.
+
+// 요소 “Jazz”, "Blues"가 있는 styles 배열을 생성합니다.
+// "Rock-n-Roll"을 배열 끝에 추가합니다.
+// 배열 정 중앙에 있는 요소를 "Classics"로 바꿉니다. 가운데 요소를 찾는 코드는 요소가 홀수 개인 배열에서도 잘 작동해야 합니다.
+// 배열의 첫 번째 요소를 꺼내서 출력합니다.
+// "Rap"과 "Reggae"를 배열의 앞에 추가합니다.
+// 단계를 하나씩 거칠 때마다 배열 모습은 아래와 같이 변해야 합니다.
+
+let styles = ["Jazz", "Blues"];
+styles.push("Rock-n-Roll");
+styles[Math.floor((styles.length - 1) / 2)] = "Classics";
+console.log(styles);
+styles.shift();
+styles.unshift("Rap", "Raggae");
+
+// border-left-width를 borderLeftWidth로 변경하기
+// "my-short-string"같이 여러 단어를 대시(-)로 구분한 문자열을 카멜 표기법을 사용한 문자열 "myShortString"로 변경해주는 함수를 작성해보세요.
+// 대시는 모두 지우고 각 단어의 첫 번째 글자는 대문자로 써주면 됩니다.
+function camelize(str) {
+  return str
+    .split("-") // splits 'my-long-word' into array ['my', 'long', 'word']
+    .map(
+      // capitalizes first letters of all array items except the first one
+      // converts ['my', 'long', 'word'] into ['my', 'Long', 'Word']
+      (word, index) =>
+        index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+    )
+    .join(""); // joins ['my', 'Long', 'Word'] into 'myLongWord'
+}
+
+// 특정 범위에 속하는 요소 찾기
+// 배열 arr의 요소 중 a이상 b 이하 범위에 속하는 요소만 골라 새로운 배열에 집어넣고, 해당 요소를 출력해주는 함수 filterRange(arr, a, b)를 작성해봅시다.
+
+// 새로 작성하는 함수는 기존 배열 arr을 변경하면 안 되고, 반환되는 함수는 새로운 배열이어야 합니다.
+
+function filterRange(arr, a, b) {
+  let newArr = arr.slice();
+  return newArr.filter((el) => el >= a && el <= b);
+}
+
+// 특정 범위에 속하는 요소 찾기 (배열 변경하기)
+// 배열 arr의 요소 중 a와 b 사이에 속하지 않는 요소는 삭제해주는 함수 filterRangeInPlace(arr, a, b)를 작성해보세요.
+// 배열의 모든 요소(i)는 다음 조건을 만족해야 합니다. a ≤ arr[i] ≤ b
+// 작성한 함수는 기존 배열을 변경하기만 하고 아무것도 반환하지 않아야 합니다.
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+
+    // 범위 밖의 요소를 제거함
+    if (val < a || val > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // 1과 4 사이에 있지 않은 요소는 모두 제거함
+
+alert(arr); // [3, 1]
+
+// 내림차순으로 정렬하기
+let arr = [5, 2, 1, -10, 8];
+
+arr.sort((a, b) => b - a);
+
+// 배열 복사본을 정렬하기
+
+// 문자열이 담긴 배열 arr을 복사한 다음 해당 배열을 정렬해봅시다.
+// 단 이때 arr은 변경되면 안 됩니다.
+
+// 함수 copySorted(arr)는 복사 후 정렬된 배열을 반환해야 합니다.
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert(sorted); // CSS, HTML, JavaScript
+alert(arr); // HTML, JavaScript, CSS (no changes)
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+alert(sorted);
+alert(arr);
