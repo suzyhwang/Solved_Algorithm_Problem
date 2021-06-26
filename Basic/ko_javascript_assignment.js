27da7b50d624de741d78ca551633c429b1078e02
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-06-26 23:50:06 +0900

    Created Programmers_30_12912.js

--- Basic/ko_javascript_assignment.js
+++ Basic/ko_javascript_assignment.js
@@ -754,9 +754,9 @@ function unique(arr) {
 }
 
 // Create keyed object from array
-Let’s say we received an array of users in the form {id:..., name:..., age... }.
+// Let’s say we received an array of users in the form {id:..., name:..., age... }.
 
-// Create a function groupById(arr) that creates an object from it, 
+// Create a function groupById(arr) that creates an object from it,
 // with id as the key, and array items as values.
 
 function groupById(arr) {
