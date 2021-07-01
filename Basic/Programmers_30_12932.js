c43ff0f650cc411c3c728f32deb720ec538527fc
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-07-01 22:49:47 +0900

    Created Programmers_30_12932.js

---
+++ Basic/Programmers_30_12932.js
@@ -0,0 +1,10 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12932
+// 문제 설명
+// 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
+// 제한 조건
+// n은 10,000,000,000이하인 자연수입니다.
+
+function solution(n) {
+  let arr = String(n).split("");
+  return arr.reverse((a, b) => +b - +a).map((el) => Number(el));
+}
