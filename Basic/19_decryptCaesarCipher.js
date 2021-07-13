ae8aa8db3586d59b4b654c202e26471c5b611aaf
suzyhwang <soyeongsuzyhwang@gmail.com>
2021-07-13 22:37:50 +0900

    Solved

--- Basic/19_decryptCaesarCipher.js
+++ Basic/19_decryptCaesarCipher.js
@@ -21,23 +21,27 @@
 // output = decryptCaesarCipher('zruog', 3);
 // console.log(output); // --> world
 
-function numberSearch(str) {
+function decryptCaesarCipher(str, secret) {
   // TODO: 여기에 코드를 작성합니다.
-  // 문자열을 입력받아 문자열에서 숫자를 모두 찾아 더한 뒤에 해당 값을 숫자와 공백을 제외한 나머지
-  // 문자열의 길이로 나눈 값을 정수로 반올ㄹ림하여 리턴해야 합니다
-  // 객체에 숫자 찾아서 넣어서 더해주고
-  // 숫자랑 공백 제외한 문자열의 길이를 카운트해서 객체에 넣어준다음에
-  // 그 둘을 나눠서 반올림한것 리턴
-  let obj = { number: 0, length: 0 };
-  for (let i = 0; i < str.length; i++) {
-    if (!isNaN(Number(str[i]))) {
-      obj["number"] = Number(obj["number"]) + Number(str[i]);
-    } else if (str[i] !== " " && isNaN(Number(str[i]))) {
-      obj["length"]++;
+  // 암호화된 문자열과 암호화 키를 입력받아 복호화된 문자열을 리턴
+  // 카이사르 암호는 평문을 암호키 secret개 만큼 오른쪽으로 평행이동시켜 암호화합니다.
+  // 복호화는 암호화된 문자열을 원래의 평문으로 복원하는 것을 말합니다.
+  // abcdefghijklmnopqrstuvwxyz
+  // hello를 3이동
+  // indexOf 써서 찾고 그거 -3으로 바꾸기
+  // d --> 3
+  // s --> 18
+  // 전체 --> 25
+  let newStr = str.split("");
+  let abc = "abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < newStr.length; i++) {
+    if (abc.indexOf(newStr[i]) !== -1) {
+      if (abc.indexOf(newStr[i]) - secret < 0) {
+        newStr[i] = abc[abc.indexOf(newStr[i]) + 26 - secret];
+      } else {
+        newStr[i] = abc[abc.indexOf(newStr[i]) - secret];
+      }
     }
   }
-  if (str.length === 0) {
-    return 0;
-  }
-  return Math.round(obj["number"] / obj["length"]);
+  return newStr.join("");
 }
